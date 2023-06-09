753f878465fedc9cfd6d596efb5ae53cf8011c27
Gcina123 <136065186+Gcina123@users.noreply.github.com>
2023-06-09 10:39:12 +0200

    Update Streamlit_app.py
    import streamlit
    
    streamlit.title('My Parents new Healty Diner')
    streamlit.h1('My Parents new Healty Diner')
    streamlit.p('My Parents new Healty Diner')
    streamlit.p('My Parents new Healty Diner')

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 
 streamlit.title('My Parents new Healty Diner')
+streamlit.h1('My Parents new Healty Diner')
+streamlit.p('My Parents new Healty Diner')
+streamlit.p('My Parents new Healty Diner')
