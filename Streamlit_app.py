a5fd1717e16ec3a5b93ef0072ee59caf772a9eda
Gcina123 <136065186+Gcina123@users.noreply.github.com>
2023-06-09 10:53:16 +0200

    Update Streamlit_app.py
    import streamlit as st
    
    st.title('My Parents New Healthy Diner')
    st.markdown('Breakfast Menu Subtitle')
    st.markdown('Omega 3 & Blueberry Oatmeal')
    st.markdown('Kale, Spinach & Rocket Smoothie')
    st.markdown('Hard-Boiled Free-Range egg')

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit as st
 
 st.title('My Parents New Healthy Diner')
-st.markdown('Breakfast Menu')
+st.markdown('Breakfast Menu Subtitle')
 st.markdown('Omega 3 & Blueberry Oatmeal')
 st.markdown('Kale, Spinach & Rocket Smoothie')
 st.markdown('Hard-Boiled Free-Range egg')
+
 
