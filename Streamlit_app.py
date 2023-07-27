212f030979384d8f025a12152e2f67e2edfba3c9
Gcina123 <136065186+Gcina123@users.noreply.github.com>
2023-07-27 16:15:38 +0200

    Update Streamlit_app.py
    r = requests.get('https://api.github.com/user', auth=('user', 'pass'))
    r.status_code
    200
    r.headers['content-type']
    'application/json; charset=utf8'
    r.encoding
    'utf-8'
    r.text
    '{"type":"User"...'
    r.json()
    {'private_gists': 419, 'total_private_repos': 77, ...}

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -22,3 +22,16 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(my_fruit_list)
+
+
+r = requests.get('https://api.github.com/user', auth=('user', 'pass'))
+r.status_code
+200
+r.headers['content-type']
+'application/json; charset=utf8'
+r.encoding
+'utf-8'
+r.text
+'{"type":"User"...'
+r.json()
+{'private_gists': 419, 'total_private_repos': 77, ...}
