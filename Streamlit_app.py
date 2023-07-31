8089037399ff8b10490ea749985c1fae3e021baf
Gcina123 <136065186+Gcina123@users.noreply.github.com>
2023-07-31 15:10:35 +0200

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -2,7 +2,6 @@ import streamlit
 import pandas
 import requests
 
-
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
