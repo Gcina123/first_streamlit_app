a31ad9b04f7f96136119237924c3d4a01ea3d2cc
Gcina123 <136065186+Gcina123@users.noreply.github.com>
2023-07-31 14:53:35 +0200

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 import pandas
 import requests
+import snowflake.connector 
 
 
 streamlit.title('My Parents New Healthy Diner')
@@ -40,7 +41,7 @@ streamlit.write('The user entered ', fruit_choice)
 
 
 
-import snowflake.connector 
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
